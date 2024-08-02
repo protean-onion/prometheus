a96520ff540948e0ed4ff85e86186adeb9f5db97
protean-onion <verma.aditya.415@gmail.com>
2024-08-03 00:03:18 +0530

    Final Settings

--- system_management/scripts/system_setup/create_sim_links.sh
+++ system_management/scripts/system_setup/create_sim_links.sh
@@ -1,3 +1,4 @@
 ln -s ~/.prometheus/system_management/config/polybar ~/.config/polybar
 ln -s ~/.prometheus/system_management/config/i3 ~/.config/i3
 ln -s ~/.prometheus/system_management/config/rofi ~/.config/rofi
+ln -s ~/.prometheus/system_management/config/alacritty ~/.config/alacritty
